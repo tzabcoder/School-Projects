921640745010d6c3f6888b65b8643f004d25a2c7
tzabcoder <60833046+tzabcoder@users.noreply.github.com>
2021-10-19 22:12:43 -0400

    Add files via upload

--- Shared_IPC/main.c
+++ Shared_IPC/main.c
@@ -1,63 +1,111 @@
-#include <stdio.h>       //Input / Output
-#include <stdlib.h>      //Handles standard library 
-#include <errno.h>       //Error handling on system calls
-#include <sys/types.h>   //System defined types
-#include <sys/ipc.h>     //IPC permission structure  
-#include <sys/shm.h>     //Shared memory requests
+#include <string.h>	  //strcpy
+#include <stdio.h>        //Input / Output
+#include <stdlib.h>	  //Handles standard library
+#include <errno.h>        //Error handling on system calls
+#include <sys/types.h>    //System defined types
+#include <sys/ipc.h>	  //IPC Permission
+#include <sys/shm.h>	  //Shared memory requests
 
-#define SHMKEY 0x77000 + 01   //Shared Memory Key
-#define STDAMNT 30            //Indexed @ 0, there are 30 users (29 students and Dr. Chen)
+//Program Definitions
+#define SHMKEY 0x77000 + 01  //Shared memory key
+#define STDAMNT 30           //Indexed at 0, there are 30 users, including Dr. Chen
 
-typedef struct{
-    char id[20];
-    char firstname[30];
-    char lastname[30];
-    int age;
-    double gpa;
-    int curactive;
+typedef struct {
+        char id[20];
+        char firstname[30];
+        char lastname[30];
+        int age;
+        double gpa;
+        int curactive;
 
-    char lastlogged[20];   //Format - YYYY:MM:DD:HH:MM:SS
-    char tottime[15];      //Format - DDD:HH:MM:SS
-
+        char lastlogged[20];     //Format - YYYY:MM:DD:HH:MM:SS
+        char tottime[15];        //Format - DDD:HH:MM:SS
 } stud;
 
 struct sharedmem_struct {
-    stud students[STDAMNT];
+        stud students[STDAMNT];
 };
 
-int main() {
+//Program Functions
+//Calculate the Average GPA
+double average_gpa(double gpas[]){
+    double total_gpa = 0;
+    double avg_gpa;
+    int i = 0;
 
-    int shm_id, i;    
-    struct sharedmem_struct *z;       //z points to shared memory array
+    for (i = 0; i < STDAMNT; i++) {
+        total_gpa += gpas[i];
+    }
+
+    avg_gpa = total_gpa / STDAMNT;
+
+	return avg_gpa;
+}
+
+//Calculate the Average Age
+double average_age(int ages[]) {
+    int total_ages = 0;
+    double avg_age;
+    int i = 0;
+
+    for (i = 0; i < STDAMNT; i++) {
+        total_ages += ages[i];
+    }
+
+    avg_age = total_ages / STDAMNT;
+
+    return avg_age;
+}
+
+int main () {
+
+    //Data Structures for holding user information 
+    double gpa_data[STDAMNT];
+    int age_data[STDAMNT];
+
+	int shm_id, i;
+    struct sharedmem_struct *z  ;            //*z points to the shared memory array
+    struct sharedmem_struct *student_data;   //Student information
 
     //Attempt to get the pre-existing shared memory
     shm_id = shmget(SHMKEY, 0, 0);    //Get pre-existing shared memory
-    if(shm_id == -1) {                //Check return value
-        printf("\nError: shmget() failed... errno=%i\n", errno);
-        perror("shmget()\n");
-        exit(-1);
+    if (shm_id == -1) {               //Check return value
+                printf("\nError: shmget() failed... errno=%i\n", errno);
+                perror("shmget()\n");
+                exit(-1);
     }
-    else {
-        printf("shmget() was sucsessful... shm_id=%i\n", shm_id);
+	else {
+        printf("shmget() passed... shm_id=%i\n", shm_id);
     }
 
-    //Attempt to attatch variable z to the shared memory segment 
-    z = (struct sm_struct*) shmat(shm_id, 0, SHM_RDONLY);   
-    if((int)z == -1) {
-        printf("\nError: shmat() failed... errno=%i\n", errno);
-        perror("shmat()");
-        exit(-1);
+    //Attempt to attatch variable z to shared memory segment
+    z = (struct sharedmem_struct*) shmat(shm_id, 0, SHM_RDONLY);
+    if ((int)z == -1) {
+            printf("\nError: shmat() failed... errno=%i\n", errno);
+            perror("shmat()\n");
+            exit(-1);
     }
-    //Else sucessfull
+	//Else Successful
 
-    //Access data from shared memory region
-    printf("Ready to read data from shared memory:\n");
-    //Reading data from memory (using z)
-    printf(z);
+    for (i = 0; i < STDAMNT; i++) {
+        gpa_data[i] = z->students[i].gpa;
+        age_data[i] = z->students[i].age;
+    }
 
-    putchar('\n');
+    double avg_gpa = average_gpa(gpa_data);
+    double avg_age = average_age(age_data);
 
-    //Detatch from shared memory
+    printf("_____ Class Data _____\n");
+    printf("Average GPA: %lf\n", avg_gpa);
+    printf("Average Age: %lf\n\n", avg_age);
+
+    printf("_____ User Information _____\n");
+    printf("Active Users: \n");
+    for (i = 0; i < STDAMNT; i++) {
+        printf("%s %s\n", z->students[i].firstname, z->students[i].lastname);
+    }
+    
+    //Detatch Variable from shared memory
     shmdt((void*) z);
 
     return 0;
